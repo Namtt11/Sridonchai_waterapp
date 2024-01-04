3c1c560ed3b6cf0a7ebd5db6b283d43eba99c07c
Chaow Porkaew <pchaowmobile@gmail.com>
2024-01-04 10:33:51 +0700

    update coding

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -16,6 +16,13 @@
       </ul>
     </details>
 
+    <details class="dropdown">
+      <summary class="m-1 btn">open or close</summary>
+      <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+        <li><a>Item 1</a></li>
+        <li><a>Item 2</a></li>
+      </ul>
+    </details>
   </div>
 </template>
 
