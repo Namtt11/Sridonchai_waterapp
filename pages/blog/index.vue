258fe9d4c9bbeb40ed25308c0639d135b9461211
Chaow Porkaew <pchaowmobile@gmail.com>
2024-01-04 11:07:44 +0700

    update coding

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,34 +1,17 @@
 <template>
   <div>
     blog index
-
+    
     <ul>
       <li v-for="blog in blogs">
-        <RouterLink class="link link-primary" :to="`/blog/${blog.id}`">{{ blog.title }}</RouterLink>
+        <a :href="`/blog/${blog.id }`">{{ blog.title }}</a>
       </li>
     </ul>
-
-    <details class="dropdown">
-      <summary class="m-1 btn">open or close</summary>
-      <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-      </ul>
-    </details>
-
-    <details class="dropdown">
-      <summary class="m-1 btn">open or close</summary>
-      <ul class="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li><a>Item 2</a></li>
-      </ul>
-    </details>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { Blog } from '@/domains/entities/blog'
-
 
 let blogs: Blog[] = [
   new Blog(1, "Hello"),
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    hello world
+  <div class="flex h-screen">
+    <div class="my-auto w-full mx-3 border rounded-md p-3 ">
+      <div class="text-2xl text-center font-bold">Login</div>
+
+      <label class="form-control w-full">
+        <div class="label">
+          <span class="label-text">Username</span>
+        </div>
+        <input type="text" placeholder="Username" class="input input-bordered w-full" />
+      </label>
+
+      <label class="form-control w-full">
+        <div class="label">
+          <span class="label-text">Password</span>
+        </div>
+        <input type="password" placeholder="Password" class="input input-bordered w-full" />
+      </label>
+
+      <button className="btn btn-block mt-3 bg-sky-500 text-white text-xl font-bold">Login</button>
+    </div>
+
+
   </div>
 </template>
 
@@ -8,6 +28,4 @@
 
 </script>
 
-<style>
-
-</style>
+<style></style>
