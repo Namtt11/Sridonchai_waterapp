b2d603feaa56cde15b0e7f5b0dcff9541c37eef7
Namtt11 <89740393+Namtt11@users.noreply.github.com>
2024-01-10 23:01:38 +0700

    AddBill

---
+++ pages/AddBill.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="flex h-dvh">
+    <div class=" w-full ">
+        <div class="pt-2 text-xl">
+                    <Icon name="ep:back"></Icon>
+        </div>
+
+        <div class="text-2xl text-center rounded-md border-sky-500">
+             เพิ่มบิล เดือนกุมภาพันธ์ <Icon name="material-symbols:calendar-month-outline"/>
+        </div>
+        <div class="text-2xl text-center">ข้อมูลการใช้น้ำย้อนหลัง <Icon name="ion:water-outline"/></div>
+        <div class="overflow-x-auto">
+            <table class="table table-zebra">
+                <!-- head -->
+                <thead>
+                <tr>
+                    <th>เดือน</th>
+                    <th>จำนวนการใช้น้ำ (หน่วย)</th>
+                    <th>ยอดชำระ (บาท)</th>
+                </tr>
+                </thead>
+                <tbody>
+                <!-- row 1 -->
+                <tr>
+                    <td>มกราคม</td>
+                    <td>20</td>
+                    <td>140</td>
+                </tr>
+                <!-- row 2 -->
+                <tr>
+                    <td>ธันวาคม</td>
+                    <td>15</td>
+                    <td>105</td>
+                </tr>
+                <!-- row 3 -->
+                <tr>
+                    <td>พฤษจิกายน</td>
+                    <td>17</td>
+                    <td>119</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+       
+
+
+        
+    
+    
+    
+    
+    
+    </div>
+   </div>      
+</template>
