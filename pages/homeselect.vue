98f3692858e21a1d0510576336cd9fcc1581bdc7
Chaleamkeat <124558406+Chaleamkeat@users.noreply.github.com>
2024-02-08 23:00:48 +0700

    update

--- pages/homeselect.vue
+++ pages/homeselect.vue
@@ -1,50 +1,160 @@
 <template>
-    <div class="flex h-dvh">
-      <div class="w-full">
-        <div class="flex items-stretch gap-3 p-3 mx-3">
-          <input
-            v-model="searchQuery"
-            type="text"
-            placeholder="ค้นหาบ้าน"
-            class="input input-bordered w-full max-w-xs border-sky-500"
-          />
-          <button @click="search" class="btn flex-col bg-white border-sky-500">
-            ค้นหา
-          </button>
+    <div class="flex flex-col mx-3 h-dvh relative">
+        <div class="flex flex-col sticky top-0 pt-3 bg-white">
+            <div class="flex items-stretch gap-3 p-3 mx-3">
+                <input
+                v-model="searchQuery"
+                type="text"
+                placeholder="ค้นหาบ้าน"
+                class="input input-bordered w-full max-w-xs border-sky-500"
+                />
+                <button @click="search" class="btn flex-col bg-white border-sky-500">
+                     ค้นหา
+                </button>
+            </div>
+          <VueDatePicker v-model="month" month-picker @closed="selectedDate" />
+          <div class="mt-3">
+            <button
+              @click="toggleSelectedStatus(status)"
+              :class="[toggleStatus(status)]"
+              class="btn py-1 px-2 mr-2"
+              v-for="status in statusFilter"
+              v-bind:key="status"
+            >
+              {{ status }}
+            </button>
+          </div>
         </div>
-        <div class="m-3">
-          <div v-for="(user, index) in users" :key="index" class="m-3 p-3 border rounded-md border-sky-500">
-            <p><b>ชื่อผู้ใช้น้ำ</b> {{ user.name }}</p>
-            <p><b>ที่อยู่</b> {{ user.address }}</p>
-            <p><b>ค้างชำระ</b> {{ user.amount }} บาท</p>
-            <p><b>หมายเลขมิเตอร์ประจำเดือน</b> {{ user.meterNumber }}</p>
+    
+        <div class="overflow-y-auto mt-3 h-full">
+          <div class="grid gap-2">
+            <div
+              v-bind:key="data.name"
+              v-for="data in reportFilteredData"
+              :class="[backgroundColor(data.status)]"
+              class="flex-col border-solid border-2 border-gray-300 p-3"
+            >
+              <div class="text-md font-bold">{{ data.owner_name }}</div>
+              <div>{{ data.house_number }}</div>
+              <span class="align-text-bottom">
+                <span>สถานะ</span>
+                <span
+                  :class="[textColor(data.status)]"
+                  class="ml-1 text-md font-bold"
+                  >{{ data.status }}</span
+                >
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </template>
+    
+          
+      
+  </template> 
   
   <script setup lang="ts">
   const searchQuery = ref('');
-  const users = ref([
+  import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import { ref } from "vue";
+import { type ReportHouse } from "@/domains/types/reportHouse";
+
+const month = ref({
+  month: new Date().getMonth(),
+  year: new Date().getFullYear(),
+});
+
+const selectedDate = () => {
+  console.log(month);
+  console.log(month.value.year);
+  let curMonth = ("0000000000" + (month.value.month + 1)).slice(-2);
+  console.log(curMonth);
+
+  let resultMonth = month.value.year + "-" + curMonth;
+  console.log(resultMonth);
+
+  loadReportHouse(resultMonth)
+};
+
+function backgroundColor(status: string) {
+  if (status === "เก็บเงินแล้ว") {
+    return "bg-green-100";
+  } else if (status === "จดมิเตอร์แล้ว") {
+    return "bg-yellow-100";
+  } else {
+    return "bg-red-100";
+  }
+}
+
+function textColor(status: string) {
+  if (status === "เก็บเงินแล้ว") {
+    return "text-green-500";
+  } else if (status === "จดมิเตอร์แล้ว") {
+    return "text-yellow-500";
+  } else {
+    return "text-red-500";
+  }
+}
+
+const statusFilter = ["เก็บเงินแล้ว", "จดมิเตอร์แล้ว", "ยังไม่ได้ดำเนินการ"];
+
+const selectedStatus = ref([
+  "เก็บเงินแล้ว",
+  "จดมิเตอร์แล้ว",
+  "ยังไม่ได้ดำเนินการ",
+]);
+
+function toggleSelectedStatus(status: string) {
+  if (selectedStatus.value.find((x: string) => x === status)) {
+    selectedStatus.value.splice(selectedStatus.value.indexOf(status), 1);
+  } else {
+    selectedStatus.value.push(status);
+  }
+}
+
+function toggleStatus(status: string) {
+  if (selectedStatus.value.find((x: string) => x === status)) {
+    return ["bg-green-200", "text-black"];
+  } else {
+    return "bg-gray-200";
+  }
+}
+
+// const reportData = ref(result.data.value?.message);
+const reportData = ref([] as ReportHouse[]);
+
+const loadReportHouse = async (sel_month : string) => {
+  let result = await useFetch<{ message: ReportHouse[] }>(
+    "http://localhost:8000/api/method/sridonchai.sridonchai.api.get_house_status_by_month",
     {
-      name: 'นายสมชาย แมนมาก',
-      address: '389 ม.3 อ.เมือง จ.พะเยา',
-      amount: 100,
-      meterNumber: 789,
-    },
-    {
-      name: 'นายสม หมา',
-      address: '389 ม.3 อ.เมือง จ.พะเยา',
-      amount: 100,
-      meterNumber: 789,
-    },
-    // Add more user data as needed
-  ]);
+      method: "post",
+      body: {
+        month: sel_month,
+      },
+    }
+  );
+
+  reportData.value = result.data.value?.message ?? []
+};
+
+selectedDate()
+
+const reportFilteredData = computed(() => {
+  return reportData?.value?.filter((x: ReportHouse) =>
+    selectedStatus.value.find((s: string) => s === x.status)
+  );
+});
   
   const search = () => {
     // Implement your search logic here
     // You can filter the 'users' array based on the searchQuery
   };
+  // Function to filter users based on search query
+function filterUsers(searchQuery: string): User[] {
+    return users.filter(user =>
+        user.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+}
   </script>
   
